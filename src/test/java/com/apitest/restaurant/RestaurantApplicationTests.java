c74fabb68f23ff8bc2a45c10f8655f47251b3a9e
Zpoxic <jjm03076@naver.com>
2021-11-30 09:38:54 +0900

    Initial commit

---
+++ src/test/java/com/apitest/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitest.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
